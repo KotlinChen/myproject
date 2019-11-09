c2d363c1b8896c69b50fa6da622762d0e9b30877
Grete Chen <bogiou087@gmail.com>
2019-11-09 16:11:24 +0800

    Java & Kotlin的variable,data type

--- src/main/java/com/chen/Hello.java
+++ src/main/java/com/chen/Hello.java
@@ -6,6 +6,24 @@ public class Hello {
         //new Person().hello(); //如果要重複使用，此寫法就會取不到這個物件
         Person p = new Person();
         p.hello();
+        int age = 35;
+        Integer age2 = 20;
+//        age2.intValue();
+//        age2.doubleValue();
+        System.out.println("年齡"+ age2.doubleValue() );
+        char c = 'm';
+        Character c2 = 'n';
+        byte b = 120;
+        
+        float weight = 66.5f;
+        float hight = 170f;
+        Float hight2 = 175f;
+        //hight2.intValue();
+        System.out.println(" xx : " + hight2);
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Grete";
+
     }
 
 }
--- src/main/kotlin/com/chen/Hello.kt
+++ src/main/kotlin/com/chen/Hello.kt
@@ -1,14 +1,30 @@
 package com.chen
 
+import javax.lang.model.element.NestingKind
+
 fun main() {
     //println("Hello Kotlin!!!")
 //    Human().hello()
     val h = Human()
     h.hello()
+    var age = 21
+    age = 22
+    println(age)
+    var long = 35.5
+    println(long)
+    val name = "Tom"
+    println(name)
+    val name2 = "Grete"
+    var name3 = "xxx"
+    name3 = "Joe"
+    println(name2)
+    println(name3)
+
+
 }
 
 class Human {
      fun hello() {
-         println("Hello 物件 Kotlin")
+         println(" Hello 物件 Kotlin")
      }
 }
