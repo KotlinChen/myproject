ccf58449bf0debfd22bf6d6960f1f9dfeee28728
Grete Chen <bogiou087@gmail.com>
2019-12-22 16:58:13 +0800

    BMI & 建構子 & 多載

--- src/main/kotlin/com/chen/Hello.kt
+++ src/main/kotlin/com/chen/Hello.kt
@@ -1,35 +1,22 @@
 package com.chen
 
-import javax.lang.model.element.NestingKind
-
 fun main() {
-    //println("Hello Kotlin!!!")
-//    Human().hello()
-    val h = Human(67.5f,1.7f)
-    println("Kotlin BMI: " + h.bmi())
-//    h.hello()
-//    var age = 21
-//    age = 22
-//    println(age)
-//    var long = 35.5
-//    println(long)
-//    val name = "Tom"
-//    println(name)
-//    val name2 = "Grete"
-//    var name3 = "xxx"
-//    name3 = "Joe"
-//    println(name2)
-//    println(name3)
+    val h = Human(weight = 60f,height = 1.7f)
+    println("Kotlin BMI:"+h.bmi())
+    h.hello()
 
 
 }
-
-class Human(var weight : Float,var height : Float ) {
-     fun bmi() : Float{
-         var bmi = weight / (height * height)
-         return bmi
-     }
-     fun hello() {
-         println(" Hello 物件 Kotlin")
-     }
+class Human(var weight : Float,var height : Float){
+    init{
+        println("Test Kotlin $weight")
+    }
+//    constructor(name : String,weight: Float,height: Float) : this(weight,height)
+    fun bmi() : Float{
+        val bmi = weight / (height * height)
+        return bmi
+    }
+    fun hello(){
+    println("Test Kotlin 108-12-21")
+    }
 }
